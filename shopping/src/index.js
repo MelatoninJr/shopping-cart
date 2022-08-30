c822b0d5abd611534a8ee5896746b897d2f6cb3c
Dillon M <dillon32300@gmail.com>
2022-08-29 21:02:50 -0500

    t

--- shopping/src/index.js
+++ shopping/src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import RouteSwitch from "./RouteSwitch";
 import Header from './components/header'
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <RouteSwitch />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
